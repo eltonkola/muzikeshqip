2030dd8e9e3e5d65f118489b504936d8fc88dd38
eltonkola <eltonkola@gmail.com>
2019-01-18 14:57:09 -0500

    wip

---
+++ src/main/kotlin/com/eltonkola/muzikeshqip/MuzikeshqipApplication.kt
@@ -0,0 +1,12 @@
+package com.eltonkola.muzikeshqip
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MuzikeshqipApplication
+
+fun main(args: Array<String>) {
+	runApplication<MuzikeshqipApplication>(*args)
+}
+
